96501287a58d61a0c866a9d07760ef87c67185ef
JohnEspinal <johnmespinalh@gmail.com>
2021-04-05 23:48:25 -0400

    Latest commit

--- VariousFeatures/VariousFeatures.iOS/FocusEffect.cs
+++ VariousFeatures/VariousFeatures.iOS/FocusEffect.cs
@@ -1,13 +1,55 @@
-﻿using Foundation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.ComponentModel;
 using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
 
-namespace VariousFeatures.iOS
+[assembly: ResolutionGroupName("MyCompany")]
+[assembly: ExportEffect(typeof(EffectsDemo.iOS.FocusEffect), nameof(EffectsDemo.iOS.FocusEffect))]
+namespace EffectsDemo.iOS
 {
-    class FocusEffect
+    public class FocusEffect : PlatformEffect
     {
+        UIColor backgroundColor;
+
+        protected override void OnAttached()
+        {
+            try
+            {
+                Control.BackgroundColor = backgroundColor = UIColor.FromRGB(204, 153, 255);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (Control.BackgroundColor == backgroundColor)
+                    {
+                        Control.BackgroundColor = UIColor.White;
+                    }
+                    else
+                    {
+                        Control.BackgroundColor = backgroundColor;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot set property on attached control. Error: ", ex.Message);
+            }
+        }
     }
 }
